1946adcba48130a2bf2d6f80463f8bc07711a15d
julienmalard <julien.malard@mail.mcgill.ca>
2023-05-07 16:29:13 -0400

    Tests Constellation.obtDonnéesTableau

--- test/client/testFonctionsSpéciales.jl
+++ test/client/testFonctionsSpéciales.jl
@@ -2,12 +2,35 @@ include("../utils.jl")
 
 avecServeurTest() do (port)
     Constellation.avecClient(port) do client
-        donnéesTableau = Constellation.obtDonnéesTableau(client, idTableau)
-        printf(donnéesTableau)
-        @test
-        
-        donnéesRéseau = Constellation.obtDonnéesNuée(client, idNuée)
+        idBd = Constellation.action(client, "bds.créerBd", Dict([("licence", "ODbl-1_0")]))
+        idTableau = Constellation.action(client, "bds.ajouterTableauBd", Dict([("idBd", idBd)]))
 
-        @test
+        idVariable = Constellation.action(
+            client, "variables.créerVariable", Dict([("catégorie", "numérique")])
+        )
+        Constellation.action(
+            client, 
+            "variables.ajouterNomsVariable", 
+            Dict([("id", idVariable), ("noms", Dict([("fr", "Précipitation")]))])
+        )
+        idColonne = Constellation.action(
+            client, 
+            "tableaux.ajouterColonneTableau", Dict([("idTableau", idTableau), ("idVariable", idVariable)])
+        )
+        Constellation.action(
+            client, 
+            "tableaux.ajouterÉlément", Dict([("idTableau", idTableau), ("vals", Dict([(idColonne, 12.3)]))])
+        )
+
+        # Sans spécifier la langue
+        donnéesTableau = Constellation.obtDonnéesTableau(client, idTableau)
+        println("données tableau", donnéesTableau)
+        
+        # En spécifiant la langue
+        # donnéesTableauLangue = Constellation.obtDonnéesTableau(client, idTableau, ["த", "fr"])
+        # println("données tableau langue", donnéesTableauLangue)
+
+        # donnéesRéseau = Constellation.obtDonnéesNuée(client, idNuée)
+
     end
 end
