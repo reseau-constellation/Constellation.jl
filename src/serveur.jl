8a8dd6aac98ffe8343afc6e419fe7c385976321d
julienmalard <julien.malard@mail.mcgill.ca>
2023-05-12 11:38:24 -0400

    Mise à jour ipa

--- src/serveur.jl
+++ src/serveur.jl
@@ -1,10 +1,12 @@
+import JSON
+
 function obtVersionServeur(exe::AbstractString="constl")
     readchomp(`$exe version`)
 end
 
 
 function lancerServeur(;port::Int=0, exe::AbstractString="constl", dossierOrbite::AbstractString="", dossierSFIP::AbstractString="")
-    commande = [exe, "lancer"]
+    commande = [exe, "lancer", "-m"]
     if port != 0
         push!(commande, "--port", string(port))
     end
@@ -15,12 +17,28 @@ function lancerServeur(;port::Int=0, exe::AbstractString="constl", dossierOrbite
         push!(commande, "--doss-orbite", dossierOrbite)
     end
 
-    proc = open(Cmd(commande))
+    proc = open(Cmd(commande), read=true, write=true)
+    
+    function fFermer()
+        print(proc, "\n")
+        while true
+            sortie = readline(proc)
+            if occursin("MESSAGE MACHINE :", sortie)
+                message = JSON.parse(split(sortie, "MESSAGE MACHINE :")[2])
+                if (message["type"] == "NŒUD FERMÉ")
+                    return
+                end
+            end
+        end
+    end
     while true
         sortie = readline(proc)
-        if occursin(":", sortie)
-            portFinal = parse(Int, split(sortie, ":")[2])
-            return (portFinal, ()->kill(proc))
+        if occursin("MESSAGE MACHINE :", sortie)
+            message = JSON.parse(split(sortie, "MESSAGE MACHINE :")[2])
+            if (message["type"] == "NŒUD PRÊT")
+                portFinal = message["port"]
+                return (portFinal, fFermer)
+            end
         end
     end
 end
